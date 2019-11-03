5672b0410c4453bdc6c0fbfafd431ffc984b1d76
duraes-antonio <garotoseis@gmail.com>
2019-11-03 09:34:07 -0300

    NEW: Concluídos e aceitos exercícios #514 (11/18)

---
+++ src/7_pilhas/00514/uva00514.cpp
@@ -0,0 +1,54 @@
+/* Problema #00514 – Rails
+ * Autor: Antônio Carlos Durães da Silva
+ */
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+    stack<int> pilha_carros;
+    int n_carros, carro;
+    bool linha_vazia;
+    cin >> n_carros;
+    int cont_carro;
+
+    while (n_carros > 0) {
+        linha_vazia = false;
+        cont_carro = 0;
+        /*Esvazie / inicialize a pilha de carros*/
+        pilha_carros = stack<int>();
+
+        /*Enquanto houverem carros a serem lidos*/
+        for (int i = 0; i < n_carros; ++i) {
+            cin >> carro;
+
+            /*Se o número lido sinalizar o fim de um bloco, aborte o empilhamento*/
+            if (carro == 0) {
+                cin >> n_carros;
+
+                /*Se houver um próximo bloco a ser lido, separe a saída com uma
+                 * linha em branco*/
+                cout << endl;
+
+                linha_vazia = true;
+                break;
+            }
+
+            while (cont_carro < n_carros && cont_carro != carro) {
+                if (!pilha_carros.empty() && pilha_carros.top() == carro) break;
+                pilha_carros.push(++cont_carro);
+            }
+
+            if (pilha_carros.top() == carro) pilha_carros.pop();
+        }
+
+        /* Se o empilhamento não foi quebrado por um fim de bloco, imprima o
+         * resultado*/
+        if (!linha_vazia) pilha_carros.empty() ? printf("Yes\n") : printf("No\n");
+    }
+
+
+    return 0;
+}
