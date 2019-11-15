061d109eb4eea6d0d215633897521a0e82215eb9
lukasg18 <lukas.gomes2010@gmail.com>
2019-11-15 16:09:03 -0300

    Update uva10920.cpp

--- src/3_matrizes/10920/uva10920.cpp
+++ src/3_matrizes/10920/uva10920.cpp
@@ -10,13 +10,12 @@
 
 using namespace std;
 
+// convem usar longos longos, pois os valores de
+//     // entrada e saida podem ser bastante grandes
+long long SZ, P, linha, coluna, raiz, i, j;
+
 int main()
 {
-    // convem usar longos longos, pois os valores de
-    // entrada e saida podem ser bastante grandes
-    long long SZ, P, raiz, i, j, linha, coluna;
-    long grupo;
-
     while (true)
     {
         scanf("%lld %lld", &SZ, &P);
@@ -24,39 +23,42 @@ int main()
             break;
 
         // Para descobrir em que anel estamos, pegamos a raiz quadrada de P
-        // e calculamos o próximo valor ímpar acima
+        // e calculamos o proximo valor ímpar acima
         raiz = sqrt(P);
-        if ((raiz * raiz != P) && (raiz % 2 == 1))
+        if (raiz * raiz == P && raiz % 2 == 1)
+        {
+        }
+        else if (raiz % 2 == 1)
             raiz += 2;
-        else if (raiz % 2 != 1)
+        else
             raiz++;
 
         // verifica-se o valor da raiz eh diferente de 1, para nao fazer
         // divisao por 0
+        i = raiz / 2, j = raiz / 2;
         if (raiz * raiz != 1)
         {
-            i = raiz / 2, j = raiz / 2;
-            grupo = (raiz * raiz - P) / (raiz - 1);
+            long grupo = (raiz * raiz - P) / (raiz - 1);
             switch (grupo)
             {
             case 0:
-                i -= ((raiz * raiz) - P);
+                i -= raiz * raiz - P;
                 break;
             case 1:
                 i -= raiz - 1;
-                j -= ((raiz * raiz) - P - (raiz - 1));
+                j -= (raiz * raiz - P - (raiz - 1));
                 break;
             case 2:
-                i -= (raiz - 1) - ((raiz * raiz) - P - (raiz - 1) * 2);
+                i -= (raiz - 1) - (raiz * raiz - P - (raiz - 1) * 2);
                 j -= raiz - 1;
                 break;
             case 3:
-                j -= (raiz - 1) - ((raiz * raiz) - P - (raiz - 1) * 3);
+                j -= (raiz - 1) - (raiz * raiz - P - (raiz - 1) * 3);
                 break;
             }
         }
-        linha = (i + SZ / 2 + 1);
-        coluna = (j + SZ / 2 + 1);
-        printf("Linha = %lld, Coluna = %lld.\n", linha, coluna);
+        linha = i + SZ / 2 + 1;
+        coluna = j + SZ / 2 + 1;
+        printf("Line = %lld, column = %lld.\n", linha, coluna);
     }
 }
