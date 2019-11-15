39801dca5cad3ffb02da35f03d5566b6889e40b3
duraes-antonio <garotoseis@gmail.com>
2019-11-15 00:10:38 -0300

    FIX: Nome var

--- src/1_warmup/12247/uva12247.cpp
+++ src/1_warmup/12247/uva12247.cpp
@@ -16,35 +16,35 @@ const bool Contem(vector<int> &Vec, const int &Element) {
 }
 
 int main() {
-    vector<int> princesa(3), princepe(2), todaEntrada;
+    vector<int> princesa(3), principe(2), todaEntrada;
     int a, b, c, x, y, saida;
 
     while (cin >> a >> b >> c >> x >> y, a) {
-        princesa.clear(); princepe.clear();
+        princesa.clear(); principe.clear();
 
         saida = 53;
 
         princesa.push_back(a);
         princesa.push_back(b);
         princesa.push_back(c);
-        princepe.push_back(x);
-        princepe.push_back(y);
+        principe.push_back(x);
+        principe.push_back(y);
 
         sort(princesa.begin(), princesa.end());  //ordena os valores
-        sort(princepe.begin(), princepe.end());  //ordena os valores
+        sort(principe.begin(), principe.end());  //ordena os valores
 
         todaEntrada(princesa);
-        todaEntrada.insert(todaEntrada.end(), princepe.begin(), princepe.end()); //Adiciona todas as entradas em um vetor
+        todaEntrada.insert(todaEntrada.end(), principe.begin(), principe.end()); //Adiciona todas as entradas em um vetor
 
-        if (princepe[0] > princesa[2]) {  //verifica se todas as cartas do principe são maiores do que as da princesa
+        if (principe[0] > princesa[2]) {  //verifica se todas as cartas do principe são maiores do que as da princesa
             for (int i = 1; i <= 52 && i < saida; ++i)
                 if (!Contem(todaEntrada, i)) saida = i;
         }
-        if (princepe[1] > princesa[2]) { // verifica se a maior carta do principe é maior que todas da princesa
+        if (principe[1] > princesa[2]) { // verifica se a maior carta do principe é maior que todas da princesa
             for (int i = princesa[2] + 1; i <= 52 && i < saida; ++i)
                 if (!Contem(todaEntrada, i)) saida = i;
         }
-        if (princepe[0] > princesa[1]) { // verifica se o primeiro do principe é maior do que duas cartas da princesa
+        if (principe[0] > princesa[1]) { // verifica se o primeiro do principe é maior do que duas cartas da princesa
             for (int i = princesa[1] + 1; i <= 52 && i < saida; ++i)
                 if (!Contem(todaEntrada, i)) saida = i;
         }
